ec671dcd265dc58920b4f111b289a5e4cf6a8a56
osmanmahmutcepoglu <lce_blue@hotmail.com>
2021-07-14 21:11:10 +0300

    14.07.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem Bakımda";
         public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError ="Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+        internal static string CategoryLimitExceded="Kategory limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
